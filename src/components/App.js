3345aa6bca2dcfa92d4f53f5146d3407630e4fd7
janekbartnicki <jachu01@poczta.onet.pl>
2022-09-29 21:10:20 +0200

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 export default class App extends Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            
+            headers: {
+                Authorization: 'Client-ID Zho4rglU8-6nRm_CJduROto1eQ_aukSz24KJpe7-bPY',
+            }
+        });
+        
+        console.log(response.data.results);
     }
 
     render() {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class SearchBar extends Component{
     state = {term: ''};
 
-    onFormSubmit = (event) => {
+    onFormSubmit = event => {
         event.preventDefault();
         this.props.onSubmit(this.state.term)
     }
@@ -12,7 +12,7 @@ export default class SearchBar extends Component{
         return (
             <form onSubmit={this.onFormSubmit}>
                 <div className="ui fluid action input">
-                    <input type="text" value={this.state.term} placeholder="Image search..." onChange={(event) => this.setState({term: event.target.value})}/>
+                    <input type="text" value={this.state.term} placeholder="Image search..." onChange={event => this.setState({term: event.target.value})}/>
                     <button className="ui button">Search</button>
                 </div>
             </form>
